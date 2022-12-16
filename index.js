778eb3b2bd79641729586e6b57db04309aef7c89
Marlowe Recht <recht.marlowe@gmail.com>
2022-12-16 15:43:17 +0100

    commit tree 74085886c798fb91bfdb3c9116f8bc3bd4956035

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const messages = [];
+
+const friends = ["Charlie", "Samip", "Ali"];
+
+function writeCards(array, celebration) {
+    for (let i = 0; i < array.length; i++) {
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${celebration} gift!`);
+    }
+    return messages;
+}
+
+function countDown(integer) {
+    let number = integer;
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
